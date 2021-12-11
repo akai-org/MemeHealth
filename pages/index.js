a210d050abadd7990d0e69ea34a117ad0a0088b8
Krychaxp <krychaxp@op.pl>
2021-12-11 11:09:08 +0100

    add swipe memes

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,31 @@
+import Image from "next/image";
+import { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { useLocalStorage } from "react-use";
+
 export default function Home() {
-  return <div>sdf</div>;
+  const [date, setDate] = useState(() => Date.now());
+  const [score, setScore] = useLocalStorage("score", { left: 0, right: 0 });
+  const updateDate =
+    ({ left = 0, right = 0 }) =>
+    () => {
+      setScore(() => ({
+        left: score.left + left,
+        right: score.right + right,
+      }));
+      setDate(Date.now());
+    };
+
+  console.log("score :>> ", score);
+  return (
+    <div id="container">
+      <button data-left onClick={updateDate({ left: 1 })}>
+        <BsChevronCompactLeft />
+      </button>
+      <img src={`/api/memes?ts=${date}`} alt="img" layout="fill" />
+      <button data-right onClick={updateDate({ right: 1 })}>
+        <BsChevronCompactRight />
+      </button>
+    </div>
+  );
 }
